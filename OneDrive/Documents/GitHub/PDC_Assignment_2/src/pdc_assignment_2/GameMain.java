223b932e65d0e8ed5c1b1dc426223199d44f3236
Scoobiboi <102942075+Scoobiboi@users.noreply.github.com>
2022-05-30 21:26:52 +1200

    More minor details

--- OneDrive/Documents/GitHub/PDC_Assignment_2/src/pdc_assignment_2/GameMain.java
+++ OneDrive/Documents/GitHub/PDC_Assignment_2/src/pdc_assignment_2/GameMain.java
@@ -1,4 +1,3 @@
-
 package pdc_assignment_2;
 
 import java.awt.Color;
@@ -9,20 +8,20 @@ import javax.swing.JFrame;
  * @author Luke
  */
 public class GameMain {
+
     public static void main(String[] args) {
         GameView view = new GameView();
         GameModel model = new GameModel();
         GameData data = new GameData();
-        
+
         JFrame frame = new JFrame("Chop Chop");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(view);
         frame.pack();
         frame.setVisible(true);
-        
         GameController controller = new GameController(view, model, frame);
         model.addObserver(view);
 
     }
-    
+
 }
